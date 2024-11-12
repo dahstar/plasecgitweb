fe6b4832272b7a7a37dcdd6e53d7bd172f498c89
dahstar <davood.abbaspour@gmail.com>
2024-11-12 12:54:30 +0330

    0.3

--- mychatapp/settings.py
+++ mychatapp/settings.py
@@ -36,8 +36,8 @@ LOGGING = {
     },
 }
 BASE_DIR = Path(__file__).resolve().parent.parent
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 
 # Quick-start development settings - unsuitable for production
