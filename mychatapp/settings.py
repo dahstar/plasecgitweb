27f7ac6c0fbd9b3d528e39117d65def374c9e973
dahstar <davood.abbaspour@gmail.com>
2024-11-13 12:39:30 +0330

    plasec 0.3

--- mychatapp/settings.py
+++ mychatapp/settings.py
@@ -104,7 +104,7 @@ WSGI_APPLICATION = 'mychatapp.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'telegram/db.sqlite3',
     }
 }
 
