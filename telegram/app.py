cb10d1b1f36b099ecdb30b80412ee56f0730e4c0
dahstar <davood.abbaspour@gmail.com>
2024-11-14 13:04:21 +0330

    plasec 0.3

--- telegram/app.py
+++ telegram/app.py
@@ -19,6 +19,19 @@ def handle_start(message):
         bot.send_message(chat_id, f"Welcome! Your /start message ID is: {start_message_id}")
     except Exception as e:
         print(f"An error occurred: {e}")
+@bot.message_handler(commands=['play'])
+def handle_start(message):
+    try:
+        # Get the message_id of the /start message
+        start_message_id = message.message_id
+        chat_id = message.chat.id  # Chat ID where the message was sent
 
+        # Print debug information to the console
+        print(f"Received /play command in chat {chat_id} with message_id {start_message_id}")
+
+        # Respond to the user
+        bot.send_message(chat_id, f"Welcome! Your /play message ID is: {start_message_id}")
+    except Exception as e:
+        print(f"An error occurred: {e}")
 # Start polling
 bot.infinity_polling()
