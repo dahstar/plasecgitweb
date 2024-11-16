4267607bed08248e7c4d0eeffcfee1a09469931d
dahstar <davood.abbaspour@gmail.com>
2024-11-16 19:10:08 +0330

    plasec 0.3

--- static/js/app.js
+++ static/js/app.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
     searchInput.addEventListener('input', function() {
         const filter = searchInput.value.toLowerCase();
         const messages = document.querySelectorAll('.message');
-        
+        if(message)
         messages.forEach(function(msg) {
             const text = msg.textContent.toLowerCase();
             if (text.includes(filter)) {
