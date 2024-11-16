2125c523d4e1f23e79e02be8cedb4118a9076968
dahstar <davood.abbaspour@gmail.com>
2024-11-16 12:22:06 +0330

    0.3

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,6 @@
-#!/bin/sh
-
-# Run Django migrations
-python manage.py migrate
 
 # Start Gunicorn for Django
-gunicorn --bind 0.0.0.0:8000 mychatapp.wsgi &
+gunicorn --bind 0.0.0.0:8000 mychatapp.wsgi  
 
 # Start the Telegram bot
 python telegram/app.py
