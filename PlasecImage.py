f9c118f0e1e0c90019fdd1867fb063fff2394e14
dahstar <davood.abbaspour@gmail.com>
2024-11-17 20:31:31 +0330

    plasec 0.3

--- PlasecImage.py
+++ PlasecImage.py
@@ -1,15 +1,14 @@
-import shlex
-import requests
-import json
-import metisai
 import os
 import time
+import requests
+import metisai
 
 class PlasecImage:
     def __init__(self):
         # Set up API key and bot ID from environment variables
         self.api_key = os.getenv('METIS_API_KEY')
         self.bot_id = os.getenv('METIS_BOT_ID_IMAGE')
+        self.url = ""
 
     def generate_image_with_metis(self, session_id, prompt):
         url = f"https://api.metisai.ir/api/v1/chat/session/{session_id}/message/async"
@@ -32,7 +31,7 @@ class PlasecImage:
             print("Task ID:", task_id)
             return task_id
         except requests.exceptions.RequestException as e:
-            print(f"Error: {e}")
+            print(f"Error generating task: {e}")
             return None
 
     def check_task_status(self, session_id, task_id):
@@ -46,9 +45,13 @@ class PlasecImage:
             response.raise_for_status()
             return response.json()
         except requests.exceptions.RequestException as e:
-            print(f"Error: {e}")
+            print(f"Error checking task status: {e}")
             return None
-
+    def get_prompt(self,prompt):
+   
+     prompt = "/imagine " + prompt
+     url = self.get_image(prompt)
+     return url
     def get_image(self, prompt):
         # Check if API key and bot ID are available
         if not self.api_key or not self.bot_id:
@@ -60,38 +63,40 @@ class PlasecImage:
         session_id = metisbot.create_session().id
         task_id = self.generate_image_with_metis(session_id, prompt)
         
-        if task_id:
-            print("Generated Task ID:", task_id)
-
-            # Polling the task status until it's finished
-            while True:
-                task_status = self.check_task_status(session_id, task_id)
-                if task_status:
-                    if task_status.get("status") == "FINISHED":
-                        print("Task finished!")
-                        attachments = task_status.get("message", {}).get("attachments", [])
-                        if attachments:
-                            image_url = attachments[0].get("content")
-                            return image_url
-                        else:
-                            print("No attachments found.")
-                        break
-                    elif task_status.get("status") == "FAILED":
-                        print("Task failed.")
-                        break
-                    else:
-                        print("Task is still running, checking again in 5 seconds...")
-                        time.sleep(5)
-                else:
-                    print("Failed to get task status.")
-                    break
-        else:
+        if not task_id:
             print("Failed to generate task ID.")
-        return "No URL found"
+            return None
+
+        print("Generated Task ID:", task_id)
+
+        # Polling the task status until it's finished or failed
+        while True:
+            task_status = self.check_task_status(session_id, task_id)
+            if task_status:
+                status = task_status.get("status")
+                if status == "FINISHED":
+                    print("Task finished!")
+                    attachments = task_status.get("message", {}).get("attachments", [])
+                    if attachments:
+                        image_url = attachments[0].get("content")
+                        return image_url
+
+                    else:
+                        print("No attachments found.")
+                        return None
+                elif status == "FAILED":
+                    print("Task failed.")
+                    return None
+                else:
+                    print("Task is still running, checking again in 5 seconds...")
+                    time.sleep(5)
+            else:
+                print("Failed to get task status.")
+                return None
 
 if __name__ == "__main__":
     d = PlasecImage()
-    s = input("prompt: ")
+    s = input("Prompt: ")
     prompt = "/imagine " + s
-    image_url = d.get_image(prompt)
-    print("Image URL:", image_url)
+    d.url = d.get_image(prompt)
+    print("Image URL:", d.url)
